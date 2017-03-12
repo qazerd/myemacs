0816e373b375836e64305961653df924a267cb49
Bertrand SIMON <bs@qazerd.fr>
2017-03-12 22:23:20 +0100

    suppression of tex stuff. agenda does not wk with capture

--- super-emacs/org-config.el
+++ super-emacs/org-config.el
@@ -8,7 +8,6 @@
 (add-hook 'org-babel-after-execute-hook 'org-display-inline-images 'append)
 (add-hook 'org-mode-hook (lambda () (setq ispell-parser 'tex)))
 
-(define-key global-map "\C-cc" 'org-capture)
 (setq org-todo-keywords
       '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "CANCELED")))
 (setq flyspell-issue-message-flag nil)
@@ -24,83 +23,6 @@
          (file "todo.org")
          "* TODO %?
 SCHEDULED: %t"))))
-; calling agenda
-;(defun air-pop-to-org-agenda (split)
-;  "Visit the org agenda, in the current window or a SPLIT."
-;  (interactive "P")
-;  (org-agenda-list)
- ; (when (not split)
- ;   (delete-other-windows)))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;; display/update images in the buffer after I evaluate
-;; 3. ignore tex commands
-
-;; 2. ignore message flags
-
-(defun flyspell-ignore-tex ()
-  (interactive)
-  (set (make-variable-buffer-local 'ispell-parser) 'tex))
-
-(add-hook 'org-mode-hook 'flyspell-ignore-tex)
-(add-hook 'org-mode-hook 'turn-on-org-cdlatex)
-(add-hook 'org-mode-hook 'wc-mode)
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)   ; with AUCTeX LaTeX mode
-(autoload 'reftex-mode     "reftex" "RefTeX Minor Mode" t)
-(autoload 'turn-on-reftex  "reftex" "RefTeX Minor Mode" nil)
-(autoload 'reftex-citation "reftex-cite" "Make citation" nil)
-(autoload 'reftex-index-phrase-mode "reftex-index" "Phrase mode" t)
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)   ; with AUCTeX LaTeX mode
-(add-hook 'latex-mode-hook 'turn-on-reftex)   ; with Emacs latex mode
-
-;; Make RefTeX faster
-(setq reftex-enable-partial-scans t)
-(setq reftex-save-parse-info t)
-(setq reftex-use-multiple-selection-buffers t)
-(setq reftex-plug-into-AUCTeX t)
-
-;; Make RefTeX work with Org-Mode
-;; use 'C-c (' instead of 'C-c [' because the latter is already
-;; defined in orgmode to the add-to-agenda command.
-(defun org-mode-reftex-setup ()
-  (load-library "reftex") 
-  (and (buffer-file-name)
-  (file-exists-p (buffer-file-name))
-  (reftex-parse-all))
-  (define-key org-mode-map (kbd "C-c (") 'reftex-citation))
-
-(add-hook 'org-mode-hook 'org-mode-reftex-setup)
-
-;; RefTeX formats for biblatex (not natbib)
-(setq reftex-cite-format
-      '(
-        (?\C-m . "\\cite[]{%l}")
-        (?t . "\\textcite{%l}")
-        (?a . "\\autocite[]{%l}")
-        (?p . "\\parencite{%l}")
-        (?f . "\\footcite[][]{%l}")
-        (?F . "\\fullcite[]{%l}")
-        (?x . "[]{%l}")
-        (?X . "{%l}")
-        ))
-
-(setq font-latex-match-reference-keywords
-      '(("cite" "[{")
-        ("cites" "[{}]")
-        ("autocite" "[{")
-        ("footcite" "[{")
-        ("footcites" "[{")
-        ("parencite" "[{")
-        ("textcite" "[{")
-        ("fullcite" "[{") 
-        ("citetitle" "[{") 
-        ("citetitles" "[{") 
-        ("headlessfullcite" "[{")))
-
-(setq reftex-cite-prompt-optional-args nil)
-(setq reftex-cite-cleanup-optional-args t)
 
 (setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/3.0.0/")
 (setq org-reveal-mathjax t)
