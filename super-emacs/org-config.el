a4a318d8c124cd35810d068e2fbcc49a195d1d7f
qazerd <bertrand.simon@qazerd.fr>
2017-03-23 15:24:34 +0100

    change for git repository of org-caldav

--- super-emacs/org-config.el
+++ super-emacs/org-config.el
@@ -9,13 +9,6 @@
 (add-hook 'org-babel-after-execute-hook 'org-display-inline-images 'append)
 (add-hook 'org-mode-hook (lambda () (setq ispell-parser 'tex)))
 
-(setq org-confirm-babel-evaluate nil)   ;don't prompt me to confirm everytime I want to evaluate a block
-(setq org-todo-keywords
-      '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "CANCELED")))
-(setq flyspell-issue-message-flag nil)
-
-;; configuration des calendriers ;;
-(setq org-caldav-save-directory "~/ownCloud/org")
 (setq org-directory "~/ownCloud/org")
 (setq org-default-notes-file "~/ownCloud/org/notes.org")
 (setq org-agenda-files (quote (
@@ -23,47 +16,54 @@
                                "~/ownCloud/org/pro.org"
                                "~/ownCloud/org/todo.org"
                                 )))
+(setq org-confirm-babel-evaluate nil)   ;don't prompt me to confirm everytime I want to evaluate a block
+(setq org-todo-keywords
+      '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "CANCELED")))
+(setq flyspell-issue-message-flag nil)
+
+;; configuration des calendriers ;;
+(setq org-caldav-save-directory "~/ownCloud/org")
 (setq org-caldav-files (quote (
                                "~/ownCloud/org/perso.org"
                                "~/ownCloud/org/pro.org"
-                               "~/ownCloud/org/todo.org"
+                               ;"~/ownCloud/org/todo.org"
                                 )))
 (setq org-caldav-inbox (quote (
-                               "~/ownCloud/org/perso.org"
-                               "~/ownCloud/org/pro.org"
-                               "~/ownCloud/org/todo.org"
+                               "~/ownCloud/org/fromperso.org"
+                               "~/ownCloud/org/frompro.org"
+                               ;"~/ownCloud/org/fromtodo.org"
                                 )))
 (setq org-agenda-files (quote (
                                "~/ownCloud/org/perso.org"
                                "~/ownCloud/org/pro.org"
-                               "~/ownCloud/org/todo.org"
+                               ;"~/ownCloud/org/todo.org"
                                 )))
 (setq org-icalendar-timezone "Europe/paris")
 (setq org-caldav-url "https://ncloud.zaclys.com/remote.php/dav/calendars/21366")
 (setq org-caldav-calendars  ; clalendriers
       (quote(
-        ;; (:calendar-id "bertrand-simon-perso"
-        ;;               :files ("~/ownCloud/org/perso.org")
-        ;;               :inbox ("~/ownCloud/org/perso.org"))
-        ;; (:calendar-id "bertrand-simon-pro"
-        ;;               :files ("~/ownCloud/org/pro.org")
-        ;;               :inbox ("~/ownCloud/org/pro.org"))
+        (:calendar-id "bertrand-simon-perso"
+                        :files ("~/ownCloud/org/perso.org")
+                        :inbox ("~/ownCloud/org/fromperso.org")) 
+        (:calendar-id "bertrand-simon-pro"
+                        :files ("~/ownCloud/org/pro.org")
+                        :inbox ("~/ownCloud/org/frompro.org"))
 
-        (:calendar-id "pro"
-                      :files ("~/ownCloud/org/todo.org")
-                      :inbox ("~/ownCloud/org/todo.org"))
-        ;; (:calendar-id "perso"
-        ;;               :files ("~/ownCloud/org/todo.org" "personnel")
-        ;;               :inbox ("~/ownCloud/org/todo.org" "personnel"))
-        ;; (:calendar-id "famille"
-        ;;               :files ("~/ownCloud/org/todo.org" "famille")
-        ;;               :inbox ("~/ownCloud/org/todo.org" "famille"))
-        ;; (:calendar-id "emacs"
-        ;;               :files ("~/ownCloud/org/todo.org" "emacs")
-        ;;               :inbox ("~/ownCloud/org/todo.org" "emacs"))
-        ;; (:calendar-id "associations"
-        ;;               :files ("~/ownCloud/org/todo.org" "associations")
-        ;;               :inbox ("~/ownCloud/org/todo.org" "associations"))
+        ;;(:calendar-id "pro"
+        ;;              :files ("~/ownCloud/org/todo.org")
+        ;;              :inbox ("~/ownCloud/org/todo.org"))
+         ;; (:calendar-id "perso"
+         ;;               :files ("~/ownCloud/org/todo.org" "personnel")
+         ;;               :inbox ("~/ownCloud/org/todo.org" "personnel"))
+         ;; (:calendar-id "famille"
+         ;;               :files ("~/ownCloud/org/todo.org" "famille")
+         ;;               :inbox ("~/ownCloud/org/todo.org" "famille"))
+         ;; (:calendar-id "emacs"
+         ;;               :files ("~/ownCloud/org/todo.org" "emacs")
+         ;;               :inbox ("~/ownCloud/org/todo.org" "emacs"))
+         ;;(:calendar-id "associations"
+         ;;              :files ("~/ownCloud/org/todo.org" "associations")
+         ;;              :inbox ("~/ownCloud/org/todo.org" "associations"))
         )))
 (setq org-agenda-custom-commands
       '(("P" ((tags "TIMESTAMP<=\"<now>\"")))))
--- super-emacs/repositories.el
+++ super-emacs/repositories.el
@@ -2,6 +2,7 @@
 (require 'package)
 
 ;Add melpa to list of repositories
+(add-to-list 'load-path "~/.emacs.d/org-caldav")
 (add-to-list 'package-archives
 	     '("melpa" . "http://melpa.milkbox.net/packages/") 
              t)
