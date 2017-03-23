f952f2994ef22bdd5f2c3381110e2c0a6de93419
qazerd <bertrand.simon@qazerd.fr>
2017-03-23 15:43:15 +0100

    back to working version org-config.el ?

--- super-emacs/org-config.el
+++ super-emacs/org-config.el
@@ -22,49 +22,14 @@
 (setq flyspell-issue-message-flag nil)
 
 ;; configuration des calendriers ;;
-(setq org-caldav-save-directory "~/ownCloud/org")
-(setq org-caldav-files (quote (
-                               "~/ownCloud/org/perso.org"
-                               "~/ownCloud/org/pro.org"
-                               ;"~/ownCloud/org/todo.org"
-                                )))
-(setq org-caldav-inbox (quote (
-                               "~/ownCloud/org/fromperso.org"
-                               "~/ownCloud/org/frompro.org"
-                               ;"~/ownCloud/org/fromtodo.org"
-                                )))
-(setq org-agenda-files (quote (
-                               "~/ownCloud/org/perso.org"
-                               "~/ownCloud/org/pro.org"
-                               ;"~/ownCloud/org/todo.org"
-                                )))
-(setq org-icalendar-timezone "Europe/paris")
-(setq org-caldav-url "https://ncloud.zaclys.com/remote.php/dav/calendars/21366")
-(setq org-caldav-calendars  ; clalendriers
-      (quote(
-        (:calendar-id "bertrand-simon-perso"
-                        :files ("~/ownCloud/org/perso.org")
-                        :inbox ("~/ownCloud/org/fromperso.org")) 
-        (:calendar-id "bertrand-simon-pro"
-                        :files ("~/ownCloud/org/pro.org")
-                        :inbox ("~/ownCloud/org/frompro.org"))
-
-        ;;(:calendar-id "pro"
-        ;;              :files ("~/ownCloud/org/todo.org")
-        ;;              :inbox ("~/ownCloud/org/todo.org"))
-         ;; (:calendar-id "perso"
-         ;;               :files ("~/ownCloud/org/todo.org" "personnel")
-         ;;               :inbox ("~/ownCloud/org/todo.org" "personnel"))
-         ;; (:calendar-id "famille"
-         ;;               :files ("~/ownCloud/org/todo.org" "famille")
-         ;;               :inbox ("~/ownCloud/org/todo.org" "famille"))
-         ;; (:calendar-id "emacs"
-         ;;               :files ("~/ownCloud/org/todo.org" "emacs")
-         ;;               :inbox ("~/ownCloud/org/todo.org" "emacs"))
-         ;;(:calendar-id "associations"
-         ;;              :files ("~/ownCloud/org/todo.org" "associations")
-         ;;              :inbox ("~/ownCloud/org/todo.org" "associations"))
-        )))
+ (setq org-icalendar-timezone "Europe/paris")
+ (setq org-caldav-url "https://webcloud.zaclys.com/remote.php/caldav/calendars/21366")
+ (setq org-caldav-calendars
+   '((:calendar-id "bertrand-simon-perso" :files ("~/ownCloud/org/perso.org")
+      :inbox "~/ownCloud/org/fromPerso.org")
+     (:calendar-id "bertrand-simon-pro"
+      :files ("~/ownCloud/org/pro.org")
+      :inbox "~/ownCloud/org/fromPro.org")))
 (setq org-agenda-custom-commands
       '(("P" ((tags "TIMESTAMP<=\"<now>\"")))))
 
