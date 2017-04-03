586c993ce9e60dcf4e25772b9778b03d8e23c7ee
Bertrand SIMON <bs@qazerd.fr>
2017-04-03 17:51:32 +0200

    configuration biblio

--- super-emacs/biblio-config.el
+++ super-emacs/biblio-config.el
@@ -1,5 +1,6 @@
 (autoload 'helm-bibtex "helm-bibtex" "" t)
 (setq bibtex-completion-bibliography
-      '(".emacs.d/bibliography/me.bib"
+      '("~/.emacs.d/bibliography/me.bib"
         ;".emacs.d/bibliography/other.bib"
         ))
+(setq bibtex-completion-library-path '("~/owncCloud/bibliography/pdf/"))
