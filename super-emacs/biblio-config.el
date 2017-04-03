3b061e88f87987305a3408c850e29293db60603f
Bertrand SIMON <bs@qazerd.fr>
2017-04-04 00:13:52 +0200

    adding https://github.com/politza/pdf-tools

--- super-emacs/biblio-config.el
+++ super-emacs/biblio-config.el
@@ -1,6 +1,11 @@
+
 (autoload 'helm-bibtex "helm-bibtex" "" t)
 (setq bibtex-completion-bibliography
-      '("~/.emacs.d/bibliography/me.bib"
+      '("~/.emacs.d/bibliography/bibliography.bib"
         ;".emacs.d/bibliography/other.bib"
         ))
 (setq bibtex-completion-library-path '("~/owncCloud/bibliography/pdf/"))
+
+
+(pdf-tools-install)
+;; see https://github.com/politza/pdf-tools
