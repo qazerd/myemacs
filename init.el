d4440026c3d23984e0bb0656a5ab40ddf4b5c422
Bertrand SIMON <bs@qazerd.fr>
2017-09-08 00:40:18 +0200

    bbdb-config add to init

--- init.el
+++ init.el
@@ -22,6 +22,7 @@
 (load-file "~/.emacs.d/super-emacs/key-bindings.el")
 (load-file "~/.emacs.d/super-emacs/edit_tex.el")
 (load-file "~/.emacs.d/super-emacs/helm-config.el")
+(load-file "~/.emacs.d/super-emacs/bbdb-config.el")
 (load-file "~/.emacs.d/super-emacs/mu4e-config.el")
 (load-file "~/.emacs.d/super-emacs/org-config.el")
 (load-file "~/.emacs.d/super-emacs/org-templates.el")
@@ -48,7 +49,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (org-gcal org-ref google-translate helm-bibtex helm-bibtexkey ztree yasnippet which-key wc-mode wc-goal-mode undo-tree theme-looper switch-window powerline ox-reveal org-caldav org-bullets ob-ipython neotree myterminal-controls multiple-cursors meta-presenter material-theme magit htmlize helm distinguished-theme dired-launch buffer-move auto-complete ace-jump-mode)))
+    (bbdb-csv-import bbdb-vcard bbdb org-gcal org-ref google-translate helm-bibtex helm-bibtexkey ztree yasnippet which-key wc-mode wc-goal-mode undo-tree theme-looper switch-window powerline ox-reveal org-caldav org-bullets ob-ipython neotree myterminal-controls multiple-cursors meta-presenter material-theme magit htmlize helm distinguished-theme dired-launch buffer-move auto-complete ace-jump-mode)))
  '(send-mail-function nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
